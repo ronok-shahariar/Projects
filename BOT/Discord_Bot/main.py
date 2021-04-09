3b307d364dbb7789177980ce8def988abb6301e7
Md. Shahariar Hassan Ronok <51051408+ronok-shahariar@users.noreply.github.com>
2021-04-09 16:37:38 +0600

    Update main.py

--- BOT/Discord_Bot/main.py
+++ BOT/Discord_Bot/main.py
@@ -7,4 +7,4 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Bot is ready.')
 
-client.run('ODMwMDA1NzY4NDg4ODc4MTAw.YHAZGw.MKaDsiClCnVNwc6txDkTY164hrI')
+client.run('')
